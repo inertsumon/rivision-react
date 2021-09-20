6b8b2a76f1988176b89cdc2546aac0b6837dd102
inertsumon <sumon.dhamoirhat@gmail.com>
2021-09-20 14:25:18 +0600

    Reactriv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ShowImages></ShowImages>
+      
     </div>
   );
+  
+/*   const productsArr = [
+    { name: "Iphone", price: 50000 },
+    { name: "Ipad", price: 150000 },
+    { name: "Fridge", price: 20000 },
+    { name: "Laptop", price: 80000 },
+    { name: "Bike", price: 250000 }
+  ];
+  return (
+    <div className="App">
+      {
+        productsArr.map(product => 
+          
+          <Products name={product.name} price={product.price} ></Products>
+        )
+      }
+    </div>
+  ); */
 }
+
+
+
+function ShowImages() {
+
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then(res => res.json())
+    .then(data=>setImages(data))
+  },[])
+
+  return (
+    <div>
+      <h1>Images</h1>
+      {
+        images.slice(0,20).map(image => 
+          <Display url={image.url} title={image.title}></Display>
+        )
+      }
+</div>
+
+  )
+  
+}
+
+function Display(props) {
+  const design = {
+    backgroundColor: "pink",
+    color: "blue",
+    margin: "10px",
+    padding: "10px",
+    border: "2px solid red",
+    
+  }
+  return (
+    <div style={design}>
+      <img src={props.url} alt="" />
+      <h3>Title: {props.title}</h3>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+/* function Products(props) {
+  const design = {
+    backgroundColor: "pink",
+    color: "blue",
+    margin: "10px",
+    padding: "10px",
+    border: "2px solid red",
+    
+  }
+  return (
+    <div style={design}>
+      <h2>Product Name: {props.name}</h2>
+      <h3>Product Name: {props.price}</h3>
+    </div>
+  )
+} */
+
+
 
 export default App;
